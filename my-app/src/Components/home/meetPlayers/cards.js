f52cad5866c8206f10479b5f87cb22535a2d442e
jaantusher <jaanealamtusher@gmail.com>
2019-09-04 19:40:26 +0600

    ready to deploy

--- my-app/src/Components/home/meetPlayers/cards.js
+++ my-app/src/Components/home/meetPlayers/cards.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
-import Otamendi from '../../../Resources/images/players/Otamendi.png';
+
 import PlayerCard from '../../ui/playerCard';
 
 class HomeCards extends Component {
