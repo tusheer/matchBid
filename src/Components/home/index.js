13c036720eb98f488643ab9804d95d744d955250
jaantusher <jaanealamtusher@gmail.com>
2019-09-06 19:34:08 +0600

    deploy

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -5,14 +5,14 @@ import MeetPlayers from './meetPlayers';
 import Promotion from './promotion';
 
 const Home = () => {
-    return (
-        <div className="bck_blue">
-            <Featured/>
-            <Matches/>
-            <MeetPlayers/>
-            <Promotion/>
-        </div>
-    );
+	return (
+		<div className="bck_blue ">
+			<Featured />
+			<Matches />
+			<MeetPlayers />
+			<Promotion />
+		</div>
+	);
 };
 
 export default Home;
